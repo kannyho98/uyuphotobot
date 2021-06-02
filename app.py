bd65e8d43034e8c43d5183fc88685fffd2c24bc3
kannyho98 <hajimekyy@gmail.com>
2021-06-03 07:56:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,7 @@ def handle_message(event):
 
         #user_message='圖文訊息'
     if user_message.find('各種查詢') != -1:    
-        
-        res_message = TemplateSendMessage(
-            alt_text='圖文訊息',
+ 
             template = CarouselTemplate(
                 columns=[
 #-----------------------------------------------------------------------------                    
